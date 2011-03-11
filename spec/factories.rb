75c3f122f9c3f76a6aa854e528f95cf278fb11b6
Eliot Walker <ewalks@gmail.com>
2011-03-10 18:04:02 -0600

    Finished modeling Users with passwords, added user show page

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                   "Eliot Walker"
+  user.email                  "ewalks@example.com"
+  user.password               "foobar"
+  user.password_confirmation  "foobar"
+end
